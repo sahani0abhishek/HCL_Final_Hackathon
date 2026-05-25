30e7c5e8228bbf8835f832773961c15fc1c23d3e
lucifer728 <vecna728@gmail.com>
2026-05-25 14:10:49 +0530

    Removed duplicate merged project folder

--- Retail_Ordering_web/Models/AdminLogin.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Retail_Ordering_web.Models
-{
-    public class AdminLogin
-    {
-        public string Username { get; set; } = "";
-        public string Password { get; set; } = "";
-    }
-}
--- Retail_Ordering_web/Models/InventoryUpdate.cs
+++
@@ -1,8 +0,0 @@
-namespace Retail_Ordering_web.Models
-{
-    public class InventoryUpdate
-    {
-        public int ProductId { get; set; }
-        public int QuantityChange { get; set; }
-    }
-}
